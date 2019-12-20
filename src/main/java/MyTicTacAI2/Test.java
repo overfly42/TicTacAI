9f98963b02f0108666d7716f62fc4dcc6eda8291
Christian Spreng <c.spreng@zoho.com>
2019-12-20 13:39:13 +0100

    Try to add a Test file

---
+++ src/main/java/MyTicTacAI2/Test.java
@@ -0,0 +1,16 @@
+package MyTicTacAI2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test
+{
+    public Test()
+    {
+
+    }
+    @Test
+    public void test1()
+    {
+        final App a = new App();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module MyTicTacAI2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.junit.jupiter;
 
     opens MyTicTacAI2 to javafx.fxml;
     exports MyTicTacAI2;
