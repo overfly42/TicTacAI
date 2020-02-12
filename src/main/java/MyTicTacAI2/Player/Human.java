d8e757efc53596d9b3120fc1f8dd7e4ad4b22c9d
Christian Spreng <c.spreng@zoho.com>
2020-02-12 23:30:55 +0100

    Front End for Game Controller

---
+++ src/main/java/MyTicTacAI2/Player/Human.java
@@ -0,0 +1,60 @@
+package MyTicTacAI2.Player;
+
+import MyTicTacAI2.SingleFieldState;
+import MyTicTacAI2.Interfaces.IGameController;
+import MyTicTacAI2.Interfaces.IPlayer;
+
+public class Human implements IPlayer {
+
+    @Override
+    public void introducePlayer(IPlayer palyer) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void startGame() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void updateField(SingleFieldState[][] field) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean isReadyToStart() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void startAction() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void registerController(IGameController gameController) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void unregisterController(IGameController gameController) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getId() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String toString() {
+        return "abc";
+    }
+}
--- src/main/java/MyTicTacAI2/PrimaryController.java
+++ src/main/java/MyTicTacAI2/PrimaryController.java
@@ -1,12 +1,58 @@
 package MyTicTacAI2;
 
+import java.io.Console;
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PrimaryController {
+import MyTicTacAI2.Interfaces.IObserver;
+import MyTicTacAI2.Interfaces.IPlayer;
+import MyTicTacAI2.Player.Human;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.cell.CheckBoxListCell;
+
+public class PrimaryController implements Initializable {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private ComboBox<IPlayer> PlayerASelector;
+    @FXML
+    private ComboBox<IPlayer> PlayerBSelector;
+    @FXML
+    private Spinner<Integer> NumberOfGames;
+    @FXML
+    private ListView<CheckBoxListCell<IObserver>> ObserverList;
+    @FXML
+    private Button startStop;
+    @FXML
+    private Label stateDisplay;
+    private boolean running;
+
+    public PrimaryController() {
+        running = false;
+    }
+
+    @FXML
+    private void toggleRunningState() throws IOException {
+        running = !running;
+        setObjectsEnabled();
+    }
+
+    private void setObjectsEnabled() {
+        startStop.setText(running ? "Stop" : "Start");
+        PlayerASelector.setDisable(running);
+        PlayerBSelector.setDisable(running);
+        NumberOfGames.setDisable(running);
+        ObserverList.setDisable(running);
+    }
+
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+        PlayerASelector.getItems().add(new Human());
     }
 }
